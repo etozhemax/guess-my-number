7a02d6c526bb41e8678362f5d012709c607eb1a0
Max Baranovsky <maxbaranovsky@192.168.1.141>
2025-03-18 19:11:01 +0300

    cleanup and refactoring

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const MAX_VALUE = 20;
 let viewModel = {
   againButton: null,
   checkButton: null,
+  infoMessage: null,
   inputValue: null,
   guessedValue: null,
   labelScore: null,
@@ -11,74 +12,78 @@ let viewModel = {
   body: null,
 };
 
-const gameState = {
-  score: 20,
-  highscore: 0,
-  guessedValue: 0,
-};
+const model = {
+  gameState: {
+    score: 20,
+    highscore: 0,
+    guessedValue: 0,
+  },
+  guessNumber() {
+    this.gameState.guessedValue = Math.floor(
+      Math.random() * MAX_VALUE + MIN_VALUE
+    );
+  },
+  parseContext(viewModel) {
+    viewModel.againButton = document.querySelector('.js-again');
+    viewModel.checkButton = document.querySelector('.js-check');
+    viewModel.inputValue = document.querySelector('.js-guess');
+    viewModel.infoMessage = document.querySelector('.js-message');
+    viewModel.guessedValue = document.querySelector('.js-number');
+    viewModel.labelScore = document.querySelector('.js-score');
+    viewModel.labelHighScore = document.querySelector('.js-highscore');
+    viewModel.body = document.querySelector('body');
+  },
+  bindEvents(viewModel) {
+    viewModel.againButton.addEventListener('click', () => {
+      model.gameState.score = 20;
+      model.guessNumber();
 
-function resetFrontendState() {
-  viewModel.guessedValue.textContent = '?';
-  viewModel.inputValue.value = '';
-  viewModel.checkButton.style = '';
-  viewModel.body.style = 'background-color: #222;';
+      this.resetFrontendState(viewModel);
+    });
 
-  viewModel.labelScore.textContent = gameState.score;
-  viewModel.labelHighScore.textContent = gameState.highscore;
-}
+    viewModel.checkButton.addEventListener('click', () => {
+      viewModel.guessedValue.textContent = viewModel.inputValue.value;
 
-function parseContext() {
-  viewModel.againButton = document.querySelector('.js-again');
-  viewModel.checkButton = document.querySelector('.js-check');
-  viewModel.inputValue = document.querySelector('.js-guess');
-  viewModel.guessedValue = document.querySelector('.js-number');
-  viewModel.labelScore = document.querySelector('.js-score');
-  viewModel.labelHighScore = document.querySelector('.js-highscore');
-  viewModel.body = document.querySelector('body');
-}
+      if (+viewModel.inputValue.value === model.gameState.guessedValue) {
+        if (model.gameState.score > model.gameState.highscore) {
+          model.gameState.highscore = model.gameState.score;
+        }
 
-function bindEvents() {
-  viewModel.againButton.addEventListener('click', () => {
-    gameState.score = 20;
-    guessNumber();
+        viewModel.checkButton.style = 'display: none;';
+        viewModel.body.style = 'background-color: green;';
 
-    resetFrontendState();
-  });
-
-  viewModel.checkButton.addEventListener('click', () => {
-    viewModel.guessedValue.textContent = viewModel.inputValue.value;
-    console.log(gameState);
-
-    if (+viewModel.inputValue.value === gameState.guessedValue) {
-      if (gameState.score > gameState.highscore) {
-        gameState.highscore = gameState.score;
+        viewModel.infoMessage.textContent = 'You won!';
+      } else {
+        model.gameState.score--;
       }
 
-      viewModel.checkButton.style = 'display: none;';
-      viewModel.body.style = 'background-color: green;';
-    } else {
-      gameState.score--;
-    }
+      viewModel.labelScore.textContent = model.gameState.score;
+      viewModel.labelHighScore.textContent = model.gameState.highscore;
 
-    viewModel.labelScore.textContent = gameState.score;
-    viewModel.labelHighScore.textContent = gameState.highscore;
+      if (model.gameState.score === 0) {
+        viewModel.checkButton.style = 'display: none;';
+        viewModel.body.style = 'background-color: red;';
+        viewModel.infoMessage.textContent = 'You lose!';
+      }
+    });
+  },
+  resetFrontendState(viewModel) {
+    viewModel.guessedValue.textContent = '?';
+    viewModel.inputValue.value = '';
+    viewModel.checkButton.style = '';
+    viewModel.infoMessage.textContent = 'Start guessing...';
+    viewModel.body.style = 'background-color: #222;';
 
-    if (gameState.score === 0) {
-      viewModel.checkButton.style = 'display: none;';
-      viewModel.body.style = 'background-color: red;';
-    }
-  });
-}
-
-function guessNumber() {
-  gameState.guessedValue = Math.floor(Math.random() * MAX_VALUE + MIN_VALUE);
-}
+    viewModel.labelScore.textContent = model.gameState.score;
+    viewModel.labelHighScore.textContent = model.gameState.highscore;
+  },
+};
 
 function initGame() {
-  parseContext();
-  bindEvents();
+  model.parseContext(viewModel);
+  model.bindEvents(viewModel);
 
-  guessNumber();
+  model.guessNumber();
 }
 
 initGame();
